78e5b8348f12142579c9929de78260a4a8115375
Nikhil Singh <inikhilworld@gmail.com>
2026-03-27 23:22:33 +0530

    pattern4

---
+++ pattern4.java
@@ -0,0 +1,21 @@
+// 1
+
+// 22
+
+// 333
+
+// 4444
+
+// 55555
+
+class Solution {
+    public void pattern4(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1);
+
+            }
+            System.out.println();
+        }
+    }
+}
