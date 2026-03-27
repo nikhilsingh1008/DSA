412899725cd1581b6b65103171684a429e58046c
Nikhil Singh <inikhilworld@gmail.com>
2026-03-27 23:02:06 +0530

    pattern1

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+// Given an integer n. You need to recreate the pattern given below for any value of N. 
+// Let's say for N = 5, the pattern should look like as below:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+class Solution {
+    public void pattern1(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
